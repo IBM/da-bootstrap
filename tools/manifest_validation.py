139eda5d5e62a5dfbf11552b97d78785c8ca80da
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-11-07 12:54:25 -0500

    try contributing a tool to Bob

---
+++ tools/manifest_validation.py
@@ -0,0 +1,14 @@
+"""
+Manifest validation tool
+This script can validate a manifest by checking that it has the correct structure and apply any wanted
+guardrails.
+
+"""
+
+# -------------------------------------------------------------------------
+# main
+# -------------------------------------------------------------------------
+
+print("\n")
+print("this is only a POC at this time and this is a placeholder for more to come.")
+print("\n")
